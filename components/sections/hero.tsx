a60d7e0862080fc88a33e96a8eddb366f961f28d
John Furlong <johnfurlong24@gmail.com>
2023-02-08 15:25:42 -0700

    clean padding for section media queries

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from '../../styles/Hero.module.scss'
 import { email } from '@/components/config';
 
-const hero = () => {
+const hero = ({ isHome } : any) => {
+  const scrollToBottom = () => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+  }
   return (
     <section className={styles.main}>
         <h1>Hi, my name is</h1>
@@ -11,7 +14,8 @@ const hero = () => {
         <p>
             {`I'm a software engineer specialized in building scalable systems for the web. I enjoy creating outstanding user experiences end to end, whether it's an interface or an API.`}
         </p>
-        <a href={`mailto:${email}`} className={styles["email-link"]}>
+        <a onClick={scrollToBottom} className={styles["email-link"]}>
+        {/* <a href={`mailto:${email}`} className={styles["email-link"]}> */}
             Contact Me
         </a>
     </section>
