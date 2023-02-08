4ec8f33b1ea9de152deacb433ca2f41ab2b714c1
John Furlong <johnfurlong24@gmail.com>
2023-02-08 12:40:33 -0700

    add transistions

--- custom-hooks/useOnClickOutside.js
+++ custom-hooks/useOnClickOutside.js
@@ -6,7 +6,6 @@ const useOnClickOutside = (ref, handler) => {
   useEffect(
     () => {
       const listener = event => {
-        console.log('EVENT: ', event)
         // Do nothing if clicking ref's element or descendent elements
         if (!ref.current || ref.current.contains(event.target)) {
           return;
