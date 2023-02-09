f044c862d43ba885baf0e043aaf9bc75c940f843
John Furlong <johnfurlong24@gmail.com>
2023-02-08 17:05:33 -0700

    align hero to about section y axis

--- components/layout.tsx
+++ components/layout.tsx
@@ -12,39 +12,6 @@ const layout = ({ location , children } : any) => {
     const isHome = location.pathname === '/';
     const [isLoading, setIsLoading] = useState(true);
     const [currentPathname, setCurrentPathname] = useState(isHome);
-    
-    // Sets target="_blank" rel="noopener noreferrer" on external links
-    // const handleExternalLinks = () => {
-    //     const allLinks = Array.from(document.querySelectorAll('a'));
-    //     if (allLinks.length > 0) {
-    //     allLinks.forEach(link => {
-    //         if (link.host !== window.location.host) {
-    //         link.setAttribute('rel', 'noopener noreferrer');
-    //         link.setAttribute('target', '_blank');
-    //         }
-    //     });
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     if (isLoading) {
-    //       return;
-    //     }
-
-    //     if (location.hash) {
-    //       const id = location.hash.substring(1); // location.hash without the '#'
-    //       console.log('ID:', id)
-    //       setTimeout(() => {
-    //         const el = document.getElementById(id);
-    //         if (el) {
-    //           el.scrollIntoView();
-    //           el.focus();
-    //         }
-    //       }, 0);
-    //     }
-    
-    //     handleExternalLinks();
-    // }, [isLoading]);
 
     return (
     <>
