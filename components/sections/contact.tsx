11cc273cd43e2a88f217d49ef08978a574b62b9c
John Furlong <johnfurlong24@gmail.com>
2023-02-26 13:13:41 -0700

    clean contact

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -13,15 +13,14 @@ const contact = () => {
       data-aos-duration="500"
       data-aos-once="true"
     >
-       <div className={styles["section-title"]}>
-          {/* <p className={styles["number"]}>{'04. '}</p> */}
-          <h2 className={styles["sm-title"]}>What's Next?</h2>
-       </div>
-       <h2 className={styles["title"]}>Get In Touch</h2>
-       <p className={styles["text"]}>
-          Trying to get in touch? My inbox is always open. Feel free to send me an email and I'll try my best to get back to you!</p>
-
-       <a className={styles["email-link"]} href={`mailto:${email}`}>
+      <div className={styles["section-title"]}>
+        <h2 className={styles["sm-title"]}>What's Next?</h2>
+      </div>
+      <h2 className={styles["title"]}>Get In Touch</h2>
+      <p className={styles["text"]}>
+        Trying to get in touch? My inbox is always open. Feel free to send me an email and I'll try my best to get back to you!
+      </p>
+      <a className={styles["email-link"]} href={`mailto:${email}`}>
         Say Hello
       </a>
     </section>
